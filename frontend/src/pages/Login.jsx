6a5e9e6a0a429033b0e0d0781744b9d8f2cd0bd3
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2025-03-08 23:41:04 +0530

    updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/user/login", {
+      const response = await fetch("https://recipe-application-r41c.onrender.com/user/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -23,7 +23,7 @@ const Signup = () => {
     }
   
     try {
-      const response = await fetch("http://localhost:8080/user/register", {
+      const response = await fetch("https://recipe-application-r41c.onrender.com/user/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password })
